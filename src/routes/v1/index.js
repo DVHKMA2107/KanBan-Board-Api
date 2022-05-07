10b4d3873ee40db61c6d7964f06bab6ee4bf242c
DVHKMA2107 <hoadamviet@gmail.com>
2022-05-07 23:56:05 +0700

    create crud api

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import { HttpStatusCode } from '*/utilities/constants'
 import { boardRoutes } from './board.route'
+import { columnRoutes } from './column.route'
+import { cardRoutes } from './card.route'
 
 const router = express.Router()
 
@@ -9,5 +11,9 @@ router.get('/status', (req, res) =>
 )
 
 router.use('/boards', boardRoutes)
+
+router.use('/columns', columnRoutes)
+
+router.use('/cards', cardRoutes)
 
 export const apiV1 = router
